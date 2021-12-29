69055e1b493b5810b8a3c1a7ddd93132dbbcb74a
yinka <holayinkajr@gmail.com>
2021-12-29 17:55:10 +0100

    added auth functions

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,9 @@
 package helpers
 
 import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -23,4 +26,24 @@ func HashAndSalt(pwd []byte) string {
 	HandleErr(err)
 
 	return string(hash)
+}
+
+func VerifyPwd(pwd string, pass string) map[string]interface{} {
+	err := bcrypt.CompareHashAndPassword([]byte(pass), []byte(pwd))
+
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return map[string]interface{}{"message": "Wrong password"}
+	}
+	return nil
+}
+
+func SignToken(claim uint) (token string, err error) {
+	tokenContent := jwt.MapClaims{
+		"user_id": claim,
+		"expiry":  time.Now().Add(time.Minute * 60).Unix(),
+	}
+	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
+	token, err = jwtToken.SignedString([]byte("Token"))
+
+	return
 }
